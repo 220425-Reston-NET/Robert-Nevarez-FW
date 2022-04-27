204099da11170786fe5e979621de77b93bfead9e
RobertNevarez <lilgosu999@gmail.com>
2022-04-27 13:05:37 -0400

    C# 1st Lesson

---
+++ Helloworld/ObjectAndClass.cs
@@ -0,0 +1,94 @@
+//This is how you create a class by using the class keyword
+//Public is just there to make the class avaliable for everyone
+
+public class Car
+{
+    //This is a field
+    //It is used to store information or define the current state of the object
+    //By default all fields should be private
+    private string _color;
+    
+    private string _owner;
+
+    private int _fuel;
+
+    private int _gallonPerMile;
+   
+    //This is a method
+    //A way to represent behavior/function and it will run multiple lines of code
+    //void means that the method will not return anything
+    public int TotalDistancePerFuel()
+    {
+        Console.WriteLine("Current Fuel: " + _fuel + " This is the owner: " + _owner);
+        Console.WriteLine($"Gallon Per Mile: {_gallonPerMile} This is the owner: {_owner}");
+        
+        Console.WriteLine(_fuel/_gallonPerMile + " This is how many miles");
+
+        //return keyword is what the method will give back
+
+        return _fuel/_gallonPerMile;
+    }
+        //Method can also have parameters
+        //Parameters are implemeted by adding in datatypes inside of the parenthesis
+
+    public void Sum(int num1, int num2)
+        {
+            Console.WriteLine(num1+num2);        
+        }
+    
+
+
+    //This is a constructor
+    //It is a special method that will execute whenever you create an object
+    public Car()
+    {
+        _color = "blue";
+        _fuel = 100;
+        _gallonPerMile = 5;
+        _owner = "Robert";
+    }
+
+    public Car(string p_owner)
+    {
+        _owner = p_owner;
+    }
+
+    //This is a property
+    //Gives us the flexibility to check that the data being stored is correct, let only certain things access data, makes things read or write only
+
+    public string Owner
+    {
+        //get keyword is this is how the data is going to be shared
+
+        get {return _owner + " is the owner of this car";}
+
+        //set keyword is how you change the data that is being stored
+
+        set {_owner=value + " Owner";}
+    }
+    //Lets say Fuel can only hold numbers from 0 to 100
+    public int Fuel
+    {
+        get {return _fuel;}
+        set 
+        {
+            if(value <= 100 && value >= 0)
+            {
+                _fuel = value;
+            }
+            // else if (value >= 0)
+            // {
+            //         _fuel = value;
+            // }
+            
+            else
+            {
+                _fuel = 100;
+                Console.WriteLine("Fuel can only hold from 0 to 100");
+            }
+            
+                
+        
+        }
+    }
+}
--- Helloworld/Program.cs
+++ Helloworld/Program.cs
@@ -1,2 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.Clear();
+Console.WriteLine("Hello, World!"); //Writeline will write something in the terminal
+
+Console.WriteLine("Making a car object!");
+//[Class Name] [NameOfObject] = new [Class name];
+Car carObj1 = new Car();
+
+//Referencing class members within an object
+
+int mile = carObj1.TotalDistancePerFuel();
+
+carObj1.Sum(5,10);
+
+Console.WriteLine(mile);
+
+Console.WriteLine("End of Method");
+
+string owner = "Robert";
+
+Car carobj2 = new Car(owner);
+Console.WriteLine(carobj2.Owner);
+carobj2.Owner = "Nevark";
+Console.WriteLine(carobj2.Owner);
+
+carobj2.Fuel = -100;
+Console.WriteLine(carobj2.Fuel);
